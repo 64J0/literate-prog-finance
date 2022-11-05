ae2c884707885045d0876e2b2fd8bc4539521f89
64J0 <vinigaio97@gmail.com>
2022-11-05 19:34:59 -0300

    feat: update the README with more information related to the literate programming feature in Emacs

--- finance.py
+++ finance.py
@@ -2,6 +2,17 @@ X_0=10000.0
 Y=1000.0
 H=1.01
 N=120
+# Python + Emacs:
+# ===========================
+#
+# If you want to show in the end of the execution, the result of
+# an operation, set :results value in the SRC block headers, and
+# make sure that the main function has a return statement.
+# If you want to just show a print result, set the :results output
+# in the header.
+#
+# Reference: https://emacs.stackexchange.com/a/64539
+
 def calcMoney (x0: float, y: float, h: float, nMonth: int) -> float:
   if (nMonth == 0):
     return x0
@@ -11,15 +22,15 @@ def calcMoney (x0: float, y: float, h: float, nMonth: int) -> float:
   else:
     return 0.0
 
-def main():
+def main ():
   resultMoney = calcMoney(X_0, Y, H, N)
 
-  print(f"""
+  return f"""
   Initial money: {format(X_0, '.2f')}
   Addition per month: {format(Y, '.2f')}
   Increase rate per month: {format(H, '.2f')}
   Investment time: {N} months
   Final value: {format(resultMoney, '.2f')} moneys
-  """)
+  """
 
-main()
+return main()
